b74a4529de18fc664c9bbe1c97f8d687c5eb1aef
alikhan <alihanmurat11@gmail.com>
2023-05-02 15:32:30 +0600

    PP2

--- Lab10/Phonebook/Main_code.py
+++ Lab10/Phonebook/Main_code.py
@@ -1,14 +1,14 @@
 import psycopg2
 from config import config
 
-def insert_people_list(people_list):
+def insert_people(name, surname, phone):
     sql = "INSERT INTO PhoneBook(Name, Surname, Phone) VALUES(%s, %s, %s) ON CONFLICT (Name, Surname, Phone) DO NOTHING"
     conn = None
     try:
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.executemany(sql, people_list)
+        cur.executemany(sql, [(name, surname, phone)])
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -17,6 +17,17 @@ def insert_people_list(people_list):
         if conn is not None:
             conn.close()
 
+def upload_from_csv():
+    import csv
+    with open('contact.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            name = row['name']
+            surname = row['surname']
+            phone = row['phone']
+            insert_people(name, surname, phone)
+
+        
 def delete_person(name, surname):
     sql = "DELETE FROM PhoneBook WHERE Name = %s AND Surname = %s"
     conn = None
@@ -83,14 +94,15 @@ def update_phone(name, surname, old_phone, new_phone):
             
 if __name__ == '__main__':
     print("""What do you want to do?
-1)Add Person 2)Delete Person 3)Change Name 4)Change Surname 5)Change Phone
+1)Add Person 2)Delete Person 3)Change Name
+4)Change Surname 5)Change Phone 6)Upload from csv
 """)
     option = int(input())
     if option == 1:
         name = str(input("Name: "))
         surname = str(input("Surname: "))
         phone = str(input("Phone Numeber: "))
-        insert_people_list([(name, surname, phone)])
+        insert_people(name, surname, phone)
         print("Completed!")
         
     elif option == 2:
@@ -123,6 +135,9 @@ if __name__ == '__main__':
         update_phone(name, surname, oldphone, newphone)
         print("Completed!")
         
+    elif option == 6:
+        upload_from_csv()
+        print("Completed!")
     else:
         print("Invalid option is selected")
 
